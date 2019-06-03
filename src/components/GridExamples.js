3ea26589ebf350b5bcc255802dfc41c52c936b35
François Georgy <francois.georgy@gmail.com>
2019-06-03 16:30:18 +0200

    styling

--- src/components/GridExamples.js
+++ src/components/GridExamples.js
@@ -1,35 +1,36 @@
 import React from 'react';
-import Diagram from "../../src/component/Diagram";
+// import Diagram from "../../src/component/Diagram";
 import {STYLES} from "../../src/utils/styles";
+import FD from "react-fretboard-diagram";
 
 const GridExamples = () => (
     <div id="grids" className={"debug"}>
         <div>
-            <Diagram frets={1} tuning={['E2', 'A2']} diagramStyle={STYLES.test1} debug={true} />
+            <FD.Diagram frets={1} tuning={['E2', 'A2']} diagramStyle={STYLES.test1} debug={true} />
         </div>
         <div>
-            <Diagram diagramStyle={STYLES.test2} debug={true} />
+            <FD.Diagram diagramStyle={STYLES.test2} debug={true} />
         </div>
         <div>
-            <Diagram diagramStyle={STYLES.test3} debug={true} />
+            <FD.Diagram diagramStyle={STYLES.test3} debug={true} />
         </div>
         <div>
-            <Diagram diagramStyle={STYLES.test4} debug={true} />
+            <FD.Diagram diagramStyle={STYLES.test4} debug={true} />
         </div>
         <div>
-            <Diagram diagramStyle={STYLES.test5} debug={true} />
+            <FD.Diagram diagramStyle={STYLES.test5} debug={true} />
         </div>
         <div>
-            <Diagram diagramStyle={STYLES.test6} debug={true} />
+            <FD.Diagram diagramStyle={STYLES.test6} debug={true} />
         </div>
         <div>
-            <Diagram diagramStyle={STYLES.test7} debug={true} />
+            <FD.Diagram diagramStyle={STYLES.test7} debug={true} />
         </div>
         <div className={"big"}>
-            <Diagram diagramStyle={STYLES.test8} debug={true} />
+            <FD.Diagram diagramStyle={STYLES.test8} debug={true} />
         </div>
         <div className={"big"}>
-            <Diagram />
+            <FD.Diagram />
         </div>
     </div>
 );
