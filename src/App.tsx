8199103bc9a0d197f369976ea0e669d5a43c36f0
François Georgy <francois.georgy@gmail.com>
2019-06-03 15:46:12 +0200

    migrating demo from react-fretboard-diagram (incomplete)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+// import {HashRouter as Router, Route, Switch, Link} from "react-router-dom";
+import Home from "./components/Home";
+import FD from "react-fretboard-diagram";
 
+const App: React.FC = () => (
+    <div>
+        <FD.Ping />
+        <Home />
+    </div>
+);
+
+/*
+const App: React.FC = () => (
+    <Router>
+        <div>
+            <h1>react-fretboard-diagram sample app</h1>
+            <Link className="header-link" to="/">Home</Link>&nbsp;
+            <Link className="header-link" to="/grid-examples">Grid Examples</Link>&nbsp;
+            <Link className="header-link" to="/grid-big">Grid Big</Link>&nbsp;
+            <Link className="header-link" to="/multiple-shapes">Multiple shapes</Link>&nbsp;
+            <Link className="header-link" to="/examples">Examples</Link>&nbsp;
+            <Link className="header-link" to="/mouse-support">Mouse</Link>&nbsp;
+            <Link className="header-link" to="/fretboard-api">Fretboard API</Link>&nbsp;
+            <Link className="header-link" to="/configurator">Configurator</Link>
+            <div id={"container"}>
+                <Switch>
+                    <Route exact={true} path="/" component={Home}/>
+                    <Route path="/grid-examples" component={GridExamples}/>
+                    <Route path="/grid-big" component={GridBig}/>
+                    <Route path="/multiple-shapes" component={MultipleShapes}/>
+                    <Route path="/examples" component={Examples}/>
+                    <Route path="/mouse-support" component={MouseSupport}/>
+                    <Route path="/fretboard-api" component={FretboardApi}/>
+                    <Route path="/configurator" component={Configurator}/>
+                </Switch>
+            </div>
+        </div>
+    </Router>
+);
+*/
+
+/*
 const App: React.FC = () => {
   return (
     <div className="App">
@@ -22,5 +62,6 @@ const App: React.FC = () => {
     </div>
   );
 }
+*/
 
 export default App;
